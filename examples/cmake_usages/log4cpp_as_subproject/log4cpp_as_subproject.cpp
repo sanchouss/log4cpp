f88cdf00ece1dbe04cd97bf78b614775e72e2e98
Alexander Perepelkin <sanchouss_@users.sf.net>
2026-02-10 16:22:33 +0100

    Building examples is enabled via the LOG4CPP_BUILD_EXAMPLES option. CMake usage examples are provided for both scenarios: importing a CMake-installed log4cpp library and using log4cpp as a CMake subproject. Both shared and static library variants are demonstrated.
    The CMake usage examples can be built either as part of the log4cpp library (when LOG4CPP_BUILD_EXAMPLES is enabled) or as standalone CMake applications that import or include log4cpp from their own directories.

---
+++ examples/cmake_usages/log4cpp_as_subproject/log4cpp_as_subproject.cpp
@@ -0,0 +1,17 @@
+#include <filesystem>
+#include <log4cpp/Category.hh>
+
+#include "log4cpp/PropertyConfigurator.hh"
+
+// The log4cpp CMake project is included as a subdirectory,
+// and the entire log4cpp library is built as part of this Cmake project.
+int main() {
+    std::filesystem::path initFile = std::filesystem::current_path() / "resources" / "log4cpp.properties";
+    std::string initFileName = initFile.string();
+
+    log4cpp::PropertyConfigurator::configure(initFileName);
+    log4cpp::Category &root = log4cpp::Category::getRoot();
+
+    root.info("Hello, log4cpp lib as cmake subdirectory!");
+    return 0;
+}
